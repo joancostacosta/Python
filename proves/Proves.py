bca7d585aa57df02b14ac4006b988a1e5e66f8b3
joancostacosta <joancostacosta@hotmail.com>
2020-04-06 15:31:57 +0200

    6/4

--- proves/Proves.py
+++ proves/Proves.py
@@ -1,18 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as pyplot
 
-pc = 1
-st = 0.25
-
-def ri(x):
-    return 1 - np.e ** (-((x - pc)**2)/(2*st**2))
+pc = 1      # punt central (mínim de la funció)
+st = 0.25   # desv. std. = distancia del punt central on la funció val la meitat del domini
 
 Ri = (lambda x: 1 - np.e ** (-((x - pc)**2)/(2*st**2)), 
     lambda x: ((x-pc)/st**2) * np.e ** (-((x - pc)**2)/(2*st**2)) )
 
 # Valores del eje X que toma el gráfico.
 #x = range(0, 5, 0.001)
-x = np.linspace(0, 5, 5000)
+x = np.linspace(0, pc+st*4, 5000)
 
 # Graficar funcion
 pyplot.plot(x, [Ri[0](i) for i in x])
